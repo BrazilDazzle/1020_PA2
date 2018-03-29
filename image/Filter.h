e404ac0891f22b503b8b1edbe86eb40c27e18e35
camlarson98 <huskyhockeyman@gmail.com>
2018-03-29 11:23:21 -0400

    Created Filter.h

---
+++ image/Filter.h
@@ -0,0 +1,14 @@
+#ifndef FILTER_H
+#define FILTER_H
+#include "Image.h"
+using namespace std;
+using Matrix = vector<vector<int>>;
+
+namespace Filter{
+    extern Matrix K3, K5;
+    Image& sharpen(Image&, Matrix&);
+    Pixel apply_kernel(Image&, int, int, Matrix&);
+    int clamp(int, int, int);
+}
+
+#endif
