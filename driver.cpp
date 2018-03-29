18cdf5b859d21f30d8321db38817c202d538ca31
Trey <rbrazil@clemson.edu>
2018-03-29 19:51:26 -0400

    driver.cpp with no errors

--- driver.cpp
+++ driver.cpp
@@ -14,9 +14,16 @@ int main(int argc, char const *argv[]) {
 
   // Open files
   std::ifstream in(argv[1]);
-  std::ifstream out(argv[2]);
 
-  if (!(in && out)) {
+  stringstream k3out;
+  k3out << "k3_" << argv[2];
+  stringstream k5out;
+  k5out << "k5_" << argv[2];
+
+  std::ofstream out3(k3out.str());
+  std::ofstream out5(k5out.str());
+
+  if (!(in && out3 && out5)) {
     std::cerr << "Could not open input/output\n";
     return 1;
   }
@@ -26,33 +33,31 @@ int main(int argc, char const *argv[]) {
   Image puppy(in);
   Image puppy2(in);
 
-  sstream k3out << "k3_" << out;
-  sstream k5out << "k5_" << out;
-
   //Matrix is a 2d vector of ints
   //3x3 Sharpen Filter
   Matrix k3 = {
+    { 0,-1, 0},
+    {-1, 5,-1},
     { 0,-1, 0}
-    {-1, 5,-1}
-    { 0,-1, 0}
-  }
+  };
 
   //5x5 Sharpen Filter
   Matrix k5 = {
+    { 0, 0,-1, 0, 0},
+    { 0, 0,-1, 0, 0},
+    {-1,-1, 9,-1,-1},
+    { 0, 0,-1, 0, 0},
     { 0, 0,-1, 0, 0}
-    { 0, 0,-1, 0, 0}
-    {-1,-1, 9,-1,-1}
-    { 0, 0,-1, 0, 0}
-    { 0, 0,-1, 0, 0}
-  }
+  };
   Filter::sharpen(puppy, k3);
   Filter::sharpen(puppy2, k5);
 
-  puppy.write_to(k3out);
-  puppy2.write_to(k5out);
+  puppy.write_to(out3);
+  puppy2.write_to(out5);
 
   in.close();
-  out.close();
+  out3.close();
+  out5.close();
 
   return 0;
 }
